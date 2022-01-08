398486a3e146c9f7c956e7feaba1b0c470120549
mykytazhum0823 <mykytazhum@gmail.com>
2022-01-08 18:33:07 +0100

    chage home page assets directory and css.

--- src/components/home/layouts/homepage.layout.js
+++ src/components/home/layouts/homepage.layout.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from "react";
 import { Footer, Header } from "./index";
-
+import '../assets/css/style.css';
+import '../assets/css/swiper-min.css';
+import '../assets/css/mmenu.css';
+import '../assets/css/line-awesome.min.css';
+import '../assets/css/flaticon.css';
+import '../assets/css/animate.min.css';
+import '../assets/css/owl.carousel.css';
+import '../assets/css/magnific-popup.css';
+import '../assets/css/nice-select.css';
 
 const HomepageLayout = ({ children }) => {
 
